579d61e8fe4a6bfdc8b971ac127266fd77c3267b
Arvish22 <36965524+Arvish22@users.noreply.github.com>
2024-03-20 22:12:01 +0530

    Add JUnit Tests
    Adding JUnit tests for BritishTimeConverter class

---
+++ src/test/java/britishtime/BritishTimeConverterTest.java
@@ -0,0 +1,18 @@
+package britishtime;
+
+import org.junit.Test;
+
+public class BritishTimeConverterTest {
+
+	@Test
+	public void testValidInputTime() {
+	}
+
+	@Test
+	public void testInvalidInputTime() {
+	}
+
+	@Test
+	public void testCornerCases() {
+	}
+}
