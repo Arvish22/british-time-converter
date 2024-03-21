83cfdd4d56b8ef3beae10656d5225c17706eae34
Arvish22 <36965524+Arvish22@users.noreply.github.com>
2024-03-21 10:58:30 +0530

    Add TimeConverterFactory class
    This commit adds the TimeConverterFactory class, which is responsible for providing instances of TimeConverter based on the specified format

---
+++ src/main/java/core/TimeConverterFactory.java
@@ -0,0 +1,25 @@
+package core;
+
+import britishTime.BritishTimeConverter;
+import britishTime.TimeConverter;
+
+/**
+ * Factory class for obtaining instances of TimeConverter based on the provided format.
+ */
+public class TimeConverterFactory {
+
+    /**
+     * Gets a TimeConverter instance based on the provided format.
+     * 
+     * @param format The desired format of the time converter.
+     * @return A TimeConverter instance corresponding to the specified format.
+     * @throws IllegalArgumentException If the provided format is not supported.
+     */
+    public static TimeConverter getTimeConverter(String format) {
+        if ("British".equalsIgnoreCase(format)) {
+            return new BritishTimeConverter();
+        } else {
+            throw new IllegalArgumentException("Unsupported format: " + format);
+        }
+    }
+}
